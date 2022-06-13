be9b8cc9101e47b93e981b75cbc0dd1654780085
PavanGandham <pavangandham99@gmail.com>
2022-06-13 16:34:41 +0530

    Changing jenkinsfile

--- script.groovy
+++ script.groovy
@@ -3,42 +3,42 @@ pipeline {
     stages {  
             stage ('Git-Checkout') {  
                 steps{
-                    git credentialsId: 'e6757c46-0750-4ab1-8cd4-c569efcbbdb1', url: 'https://github.com/sumyak/CI-CD-Pipeline.git'
+                    url: 'https://github.com/PavanGandham/CI-CD-Project-1.git'
                     echo "Checkout successful";
                 } 
             }
             stage ('Compile') {  
-                  steps{
-                    bat label: '', script: 'mvn compile'
+                steps{
+                    sh 'mvn compile'
                     echo "test successful";
                     
                 } 
             }
             stage ('Build') {  
-                  steps{
-                    bat label: '', script: 'mvn clean'
-                    bat label: '', script: 'mvn package'
+                steps{
+                    sh 'mvn clean'
+                    sh 'mvn package'
                     echo "build successful";
                     
                 } 
             }
-             stage ('Test') {  
-                  steps{
-                    bat label: '', script: 'mvn test'
+            stage ('Test') {  
+                steps{
+                    sh 'mvn test'
                     echo "test successful";
                 } 
             }
             
         stage ('Deploy') {
             steps{
-            deploy adapters: [tomcat9(credentialsId: '0ec60cd3-c147-467b-bc23-ceabd7954e28', path: '', url: 'http://localhost:8081/')], contextPath: 'jenkins_calci', onFailure: false, war: '**/*.war'
-             echo "Deploy successful";
+            deploy adapters: [tomcat9(credentialsId: 'admin', path: '', url: 'http://ec2-44-202-15-56.compute-1.amazonaws.com:8080/')], contextPath: 'jenkins_calci', onFailure: false, war: '**/*.war'
+            echo "Deploy successful";
             }
         }
         stage ('Monitor') { 
-           steps{ 
-             echo "Now you can monitor!";
-           }
+            steps{ 
+                echo "Now you can monitor!";
+            }
         }
     }
 }
